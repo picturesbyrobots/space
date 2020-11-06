7970a46fd2bf2c8af390e08a3aa12ed36c37d9c4
Dave Tennent <picturesbyrobots@gmail.com>
2020-11-06 10:01:19 -0500

    duplicate and spin chairs.

--- static-default/hmlt/scenes/interior.js
+++ static-default/hmlt/scenes/interior.js
@@ -31,10 +31,9 @@ export const init = (scene_root) =>
 
 
                   let [x,y,z] = offset;
-                  //  let new_dup = actor_mesh.userData.cloneActor()
-                  
-                  // scene_root.add(new_dup)
-                  // duplicates.push(new_dup)
+                    let new_dup = actor_mesh.userData.cloneActor()
+                   scene_root.add(new_dup)
+                   duplicates.push(new_dup)
                   
 
             })
@@ -42,17 +41,74 @@ export const init = (scene_root) =>
 
       }
 
-
-
       
+      const chair_offsets =
+            [
+                  [-0,25, 5],
+                  [10, 20, 10],
+                  [10,10, -5]
+
+            ]
+
+      let axis = 0;
+      chair_offsets.map(offset => {
+            let [x,y,z] = offset
+            let new_dup = scene_root.getObjectByName("chair").clone()
+            new_dup.position.set(x,y,z)
+            new_dup.userData = 
+            {
+                  animate : true,
+                  axis : axis
+            }
+            scene_root.add(new_dup)
+            axis = axis == 3 ? 0 : axis + 1;
+            
+
+
+
+
+      })
+
+
+
+     scene_root.getObjectByName("chair").userData = 
+            {
+                  animate: true,
+                  axis : 0
+            } 
 
       
 
 }
 
+const up = new THREE.Vector3(0,1,0)
+let angle = 0.0;
+let rot_speed = .01
+const spin = (obj) => {
+      switch(obj.userData.axis) {
+
+            case 0 : 
+                  obj.rotateY(rot_speed)
+                  break; 
+            case 1 :
+                  obj.rotateX(rot_speed)
+                  break
+            case 2 :
+                  obj.rotateZ(rot_speed)
+                  break
+
+
+      }
+
+}
 
 export const animate= (scene_root) => 
 {
-      //duplicates.map(dup => dup.visible = actor_mesh.getObjectByName("vince-mesh").visible)
+      duplicates.map(dup => dup.visible = actor_mesh.getObjectByName("vince-mesh").visible)
+
+      scene_root.children.map(child =>  {
+            child.userData.animate && spin(child)
+
+      })
       
 }
