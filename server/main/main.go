19e1d92400b70486e64ecde93cf71e941ef972ee
Sidney San Martín <sidney@s4y.us>
2020-11-13 10:59:40 -0500

    Fix overly aggressive caching by Safari?

--- server/main/main.go
+++ server/main/main.go
@@ -338,6 +338,7 @@ func main() {
 		fileServer := http.FileServer(http.Dir(*staticDir))
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Cache-Control", "must-revalidate")
+			w.Header().Set("Vary", "*")
 			fileServer.ServeHTTP(w, r)
 		})
 	} else {
